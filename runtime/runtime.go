a1a338ec8a1d56b440d559459fa4a87456a3f922
Zhao KK <kikakkz@hotmail.com>
2021-03-25 14:52:06 +0800

    Support to get nvme count

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,7 +1,38 @@
 package devopsruntime
 
+import (
+	"os/exec"
+	"strings"
+)
+
+type diskInfo struct {
+	name        string `json:"disk_name"`
+	description string `json:"disk_discription"`
+}
+
+func getRootPart() string {
+	out, _ := exec.Command("mount | grep \"on / type\" | awk '{print $1}'").Output()
+	return string(out)
+}
+
+func getNvmeDiskList() []string {
+	out, _ := exec.Command("lsblk | grep \"disk\" | grep \"nvme\"").Output()
+	return strings.Split(string(out), "\n")
+}
+
 func GetNvmeCount() (int, error) {
-	return 0, nil
+	nvmeList := getNvmeDiskList()
+	rootPart := getRootPart()
+
+	nvmeCount := 0
+	for _, nvme := range nvmeList {
+		if strings.Contains(rootPart, nvme) {
+			continue
+		}
+		nvmeCount += 1
+	}
+
+	return nvmeCount, nil
 }
 
 func GetNvmeDesc() ([]string, error) {
