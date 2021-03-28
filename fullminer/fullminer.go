de9be07e063e5098cafd59e9a35f35279556ac1f
Zhao KK <kikakkz@hotmail.com>
2021-03-28 15:55:04 +0800

    Add basic fullminer metrics

--- fullminer/fullminer.go
+++ fullminer/fullminer.go
@@ -2,25 +2,35 @@ package fullminer
 
 import (
 	log "github.com/EntropyPool/entropy-logger"
+	api "github.com/NpoolDevOps/fbc-devops-peer/api/lotusapi"
 	"github.com/NpoolDevOps/fbc-devops-peer/basenode"
 	devops "github.com/NpoolDevOps/fbc-devops-peer/devops"
 	exporter "github.com/NpoolDevOps/fbc-devops-peer/exporter"
+	lotusmetrics "github.com/NpoolDevOps/fbc-devops-peer/metrics/lotusmetrics"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type FullMinerNode struct {
 	*basenode.Basenode
+	*lotusmetrics.LotusMetrics
 }
 
 func NewFullMinerNode(config *basenode.BasenodeConfig, devopsClient *devops.DevopsClient) *FullMinerNode {
 	log.Infof(log.Fields{}, "create %v ndoe", config.NodeConfig.MainRole)
 	fullminer := &FullMinerNode{
 		basenode.NewBasenode(config, devopsClient),
+		lotusmetrics.NewLotusMetrics(),
 	}
 	return fullminer
 }
 
 func (n *FullMinerNode) Describe(ch chan<- *prometheus.Desc) {
+	addr, err := n.MyLocalAddr()
+	if err != nil {
+		return
+	}
+
+	api.ChainSyncState(addr)
 	log.Infof(log.Fields{}, "NOT IMPLEMENT FOR FULLMINERNODE")
 }
 
