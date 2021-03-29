a472aed1e738d6b424f0a640cf2fbda8c1293148
Zhao KK <kikakkz@hotmail.com>
2021-03-29 12:55:06 +0800

    Add ping test

--- metrics/basemetrics/basemetrics_test.go
+++ metrics/basemetrics/basemetrics_test.go
@@ -1,9 +1,12 @@
 package basemetrics
 
 import (
+	log "github.com/EntropyPool/entropy-logger"
 	"testing"
 )
 
 func TestNewBaseMetrics(t *testing.T) {
 	NewBaseMetrics()
+	delay, lost := pingStatistic("www.baidu.com")
+	log.Infof(log.Fields{}, "DELAY : %v, LOST : %v", delay, lost)
 }
