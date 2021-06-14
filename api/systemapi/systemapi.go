b2d6f54d8b01a6d8d87edb144bd28fb514e7f8ab
crazyzpl <crazyzplzpl@163.com>
2021-06-14 16:53:34 +0800

    add systemapi and move Runcommand into systemapi.go

---
+++ api/systemapi/systemapi.go
@@ -0,0 +1,15 @@
+package systemapi
+
+import (
+	"os"
+	"os/exec"
+)
+
+func RunCommand(cmd *exec.Cmd) ([]byte, error) {
+	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
