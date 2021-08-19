0b9dfe5bee69e9a23abd06f016ca8b04fd099947
crazyzpl <crazyzplzpl@163.com>
2021-08-19 19:31:40 +0800

    update

--- api/minerapi/minerapi.go
+++ api/minerapi/minerapi.go
@@ -79,34 +79,19 @@ func GetMinerInfo(ch chan MinerInfo, sectors bool) {
 			}
 			if strings.Contains(lineStr, "Power: ") {
 				info.Power, _ = strconv.ParseFloat(strings.Split(lineStr, " ")[1], 64)
-				if strings.Contains(lineStr, "Pi") {
-					info.Power = info.Power * 1024
-				}
 			}
 			if strings.Contains(lineStr, "Raw: ") {
 				info.Raw, _ = strconv.ParseFloat(strings.Split(lineStr, " ")[1], 64)
-				if strings.Contains(lineStr, "Pi") {
-					info.Raw = info.Raw * 1024
-				}
 			}
 			if strings.Contains(lineStr, "Committed: ") {
 				info.Committed, _ = strconv.ParseFloat(strings.Split(lineStr, " ")[1], 64)
-				if strings.Contains(lineStr, "Pi") {
-					info.Committed = info.Committed * 1024
-				}
 			}
 			if !inSectorState {
 				if strings.Contains(lineStr, "Proving: ") {
 					info.Proving, _ = strconv.ParseFloat(strings.Split(lineStr, " ")[1], 64)
-					if strings.Contains(lineStr, "Pi") {
-						info.Proving = info.Proving * 1024
-					}
 					if strings.Contains(lineStr, "Faulty, ") {
 						faulty := strings.Split(lineStr, "(")[1]
 						info.Faulty, _ = strconv.ParseFloat(strings.Split(faulty, " ")[0], 64)
-						if strings.Contains(lineStr, "Pi") {
-							info.Faulty = info.Faulty * 1024
-						}
 					}
 				}
 			}
@@ -138,6 +123,14 @@ func GetMinerInfo(ch chan MinerInfo, sectors bool) {
 			}
 			if strings.Contains(lineStr, "Sectors:") {
 				inSectorState = true
+			}
+
+			if strings.Contains(lineStr, "PiB") {
+				info.Power = info.Power * 1024
+				info.Raw = info.Raw * 1024
+				info.Committed = info.Committed * 1024
+				info.Proving = info.Proving * 1024
+				info.Faulty = info.Faulty * 1024
 			}
 		}
 
